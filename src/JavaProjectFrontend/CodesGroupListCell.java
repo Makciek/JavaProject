d2404e7dea15f6f733ac3ab9b80497d53c82de2d
Makciek <makciek@gmail.com>
2017-06-11 17:01:29 +0200

    before switch to accordian

--- src/JavaProjectFrontend/CodesGroupListCell.java
+++ src/JavaProjectFrontend/CodesGroupListCell.java
@@ -8,12 +8,15 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TreeCell;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 
 /**
  * Created by Makciek on 04.06.2017.
  */
 public class CodesGroupListCell extends ListCell<CodesGroup>
 {
+    final int ROW_HEIGHT = 24;
+
     @Override
     protected void updateItem(CodesGroup item, boolean empty) {
         super.updateItem(item, empty);
@@ -21,13 +24,28 @@ public class CodesGroupListCell extends ListCell<CodesGroup>
         if(item != null){
             VBox vbox = new VBox();
             Label header = new Label(item.GroupName.getValue());
+
             ListView<Code> codeListView = new ListView<Code>();
+            codeListView.setPrefHeight(ROW_HEIGHT * item.Codes.size());
+            codeListView.setPrefWidth(codeListView.getPrefWidth() - 20);
 
             codeListView.setItems(item.Codes);
+            codeListView.setCellFactory(param -> new CodeCell());
 
             vbox.getChildren().add(header);
             vbox.getChildren().add(codeListView);
             setGraphic(vbox);
+        }
+    }
+
+    private class CodeCell extends ListCell<Code> {
+        @Override
+        protected void updateItem(Code item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if(item!=null){
+                setText(item.CodeName.getValue());
+            }
         }
     }
 }
--- src/JavaProjectFrontend/Controller.java
+++ src/JavaProjectFrontend/Controller.java
@@ -5,10 +5,14 @@ import JavaProjectFrontend.Models.CodesGroup;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.SortedList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
 
 import java.util.List;
@@ -23,41 +27,44 @@ public class Controller
     @FXML
     private ListView codesListView;
 
+    @FXML
+    public TabPane codesTabPane;
+
     public Controller()
     {
-        _codeGroups = FXCollections.observableArrayList
-        (
-            new CodesGroup(
-                "1. Pierwsze laby",
-                FXCollections.observableArrayList
-                (
-                     new Code("1.1 Przykład pierwszy"),
-                     new Code("1.1 Przykład drugi")   ,
-                     new Code("1.1 Przykład trzeci")
-                )
-            ),
-            new CodesGroup(
-                "2. Drugie laby",
-                FXCollections.observableArrayList
-                (
-                     new Code("1.1 Przykład pierwszy"),
-                     new Code("1.1 Przykład drugi")   ,
-                     new Code("1.1 Przykład trzeci")
-                )
-            )
-        );
+        _codeGroups = FXCollections.observableArrayList();
+
+        for (int i=0;i<20;i++){
+            _codeGroups.add(new CodesGroup(
+                    i + ". Pierwsze laby",
+                    FXCollections.observableArrayList
+                            (
+                                    new Code(i + ".1 Przykład pierwszy"),
+                                    new Code(i + ".1 Przykład drugi")   ,
+                                    new Code(i + ".1 Przykład trzeci")
+                            )
+            ));
+        }
     }
 
     @FXML
     private void initialize()
     {
-        codesListView.setItems(_codeGroups);
-        codesListView.setCellFactory(new Callback<ListView<CodesGroup>, ListCell<CodesGroup>>() {
+        codesListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
-            public ListCell call(ListView param)
-            {
-                return new CodesGroupListCell();
+            public void handle(MouseEvent mouseEvent) {
+                if(mouseEvent.getButton().equals(MouseButton.PRIMARY)){
+                    if(mouseEvent.getClickCount() == 2){
+                        Tab tab = new Tab();
+                        tab.setText("Hello world");
+
+                        codesTabPane.getTabs().add(tab);
+                    }
+                }
             }
         });
+
+        codesListView.setItems(_codeGroups);
+        codesListView.setCellFactory(param -> new CodesGroupListCell());
     }
 }
